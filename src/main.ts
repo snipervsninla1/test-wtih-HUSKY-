3be020e9b7cf7e527618ddc0bd0e112c61361424
unknown <snipervsninla1@gmail.com>
2022-02-23 11:12:59 +0100

    19eme commit

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import App from './App.vue'
 
-createApp(App).mount("#app");
+createApp(App).mount('#app')
+
+const test = '12'
+const sd = 7
+const one = '78'
+
+const two = 79
+
+const three = '7'
