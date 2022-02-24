3af692ba897e8831fdecd7726e44df64de8fad90
unknown <snipervsninla1@gmail.com>
2022-02-24 13:54:32 +0100

    commit test after configuration

---
+++ .eslintrc1.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "plugin:vue/essential",
+  parserOptions: {
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  plugins: ["vue", "@typescript-eslint"],
+  rules: {
+    "space-before-blocks": "off",
+    "semi-style": "error",
+    "func-name-matching": "warn",
+    "dot-notation": "warn",
+    "comma-style": "warn",
+  },
+};
