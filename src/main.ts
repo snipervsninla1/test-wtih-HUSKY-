93aeb310e216ffcc853cf8e6a1d4cda982f04c50
unknown <snipervsninla1@gmail.com>
2022-02-24 14:01:53 +0100

    commit test after correct the error

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,16 @@
-import { createApp } from 'vue';
-import App from './App.vue';
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app');
+createApp(App).mount("#app");
 
-const didShoshMessage = false;
-
-function showmessage() {
-  const message = 'I like logs';
+const showMessage = () => {
+  let message = "I like logs";
   for (let i = 0; i < 5; i++) {
     console.log(message);
   }
-}
-
-showmessage();
-const test = {
-  name: 'ali',
-  age: 12,
 };
-const personne = test['name'];
-const foo = { class: 'CS 101' };
-var x = foo['class'];
+
+showMessage();
+const test = { name: "ali", age: 12 };
+const foo = { class: "CS 101" };
+const x = foo.class;
